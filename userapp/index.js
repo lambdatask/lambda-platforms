07fc6518261a609ddd841a6298e7b9411cb2ae41
Emerson Macedo <emerleite@gmail.com>
2016-07-08 20:16:33 -0300

    A real lambda example, using imput to get an URL and just print it

--- userapp/index.js
+++ userapp/index.js
@@ -1,3 +1,8 @@
-module.exports = () => {
-  return "hello world"
+const unirest = require('unirest');
+
+module.exports = (context) => {
+  const Request = unirest.get(context.url);
+  Request.end((response) => {
+    console.log(response.body)
+  })
 }
